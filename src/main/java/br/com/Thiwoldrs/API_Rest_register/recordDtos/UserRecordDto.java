5987632812a549e8a4cf126aad8422f668f6e917
Thiago <thiagofonsecaclaudino@gmail.com>
2024-08-27 02:22:16 -0300

    feat: adicionado a camada dto record de users

--- src/main/java/br/com/Thiwoldrs/API_Rest_register/recordDtos/UserRecordDto.java
+++ src/main/java/br/com/Thiwoldrs/API_Rest_register/recordDtos/UserRecordDto.java
@@ -1,4 +1,12 @@
 package br.com.Thiwoldrs.API_Rest_register.recordDtos;
 
-public record UserRecordDto() {
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserRecordDto(
+        @NotBlank (message = "O nome não pode estar em branco") String nome,
+        @NotBlank (message = "O email não pode estar em branco") String email,
+        @NotBlank (message = "A idade não pode estar em branco") Byte idade,
+        @NotBlank (message = "O nome não pode estar em branco") String cpf
+        ) {
 }
