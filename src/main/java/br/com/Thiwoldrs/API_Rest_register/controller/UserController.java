c14a4815f2908e9d712efde37ffc0d572c060cf0
Thiago <thiagofonsecaclaudino@gmail.com>
2024-08-28 02:26:22 -0300

    feat: adicionando o métodos de getAll, getOne, updateUser e deleteUser na camada do controller User

--- src/main/java/br/com/Thiwoldrs/API_Rest_register/controller/UserController.java
+++ src/main/java/br/com/Thiwoldrs/API_Rest_register/controller/UserController.java
@@ -3,28 +3,65 @@ package br.com.Thiwoldrs.API_Rest_register.controller;
 import br.com.Thiwoldrs.API_Rest_register.model.UserModel;
 import br.com.Thiwoldrs.API_Rest_register.recordDtos.UserRecordDto;
 import br.com.Thiwoldrs.API_Rest_register.repository.UserRepository;
+import br.com.Thiwoldrs.API_Rest_register.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
 
     @PostMapping("/users")
     public ResponseEntity<UserModel> create (@RequestBody @Valid UserRecordDto userRecordDto){
-        var userModel = new UserModel();
-        BeanUtils.copyProperties(userRecordDto, userModel);
-        return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userModel));
+        var createUser = userService.createUser(userRecordDto);
+        return ResponseEntity.status(HttpStatus.OK).body(createUser);
 
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserModel>> getAll (){
+        List<UserModel> users = userService.getAll();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<Object> getOne (@PathVariable (value = "id")UUID id){
+        var user = userService.getOne(id);
+
+        if(user.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("USER NOT FOUND");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(user.get());
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<Object> updateUser (@PathVariable(value = "id") UUID id,
+                                              @RequestBody @Valid UserRecordDto userRecordDto){
+
+        Optional<UserModel> userO = userService.updateUser(id, userRecordDto);
+        if (userO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("USER NOT FOUND");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(userO.get());
+    }
+
+    @DeleteMapping("users/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable (value = "id") UUID id){
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body("USER DELETED SUCESSFULLY.");
     }
 }
